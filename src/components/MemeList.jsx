86e575f407f4bda03bb225415272f20a7911dcef
LUPUS001 <antoniodaw23@gmail.com>
2026-03-06 08:29:47 +0100

    Asegurando codigo de API

--- src/components/MemeList.jsx
+++ src/components/MemeList.jsx
@@ -1,4 +1,5 @@
-import "./MemeList.css";
+import axios from "axios";
+import "./MemeList.css"; //no hace falta usarlo pero queda mejor
 import { useEffect, useState } from "react";
 
 function MemeList() {
@@ -6,12 +7,15 @@ function MemeList() {
 
   const HTMLMemes = memes.map((meme) => {
     return (
+      // Esta clase(meme-item) hace referencia a una clase de MemeList.css, no hace falta hacer esta parte, pero ya que se hace en el tutorial, pues todo suma
       <li key={meme.id} className="meme-item">
-        <h2>meme.name</h2>
+        <h2>{meme.name}</h2>
         <img src={meme.url} alt="Meme image" className="meme-img" />
       </li>
     );
   });
+
+  /* Versión con fetch */
   // Cuando se monta este componente
   useEffect(() => {
     // hacemos un fetch a la API
@@ -24,6 +28,17 @@ function MemeList() {
         setMemes(datos.data.memes);
       });
   }, []);
+
+  /* Versión con axio (hay que instalarlo con --> npm i axios) */
+  /* useEffect(() => {
+    axios.get("https://api.imgflip.com/get_memes").then((response) => {
+      // A axios no le hace falta transformar el json que recibimos de la api imgflip, por lo que nos saltamos el paso de...
+      // .then((response) => response.json())
+
+      console.log(response.data); //este response es equivalente al datos de ".then((datos) => {}"
+      setMemes(response.data.data.memes);
+    });
+  }, []); */
 
   return <ul className="meme-list">{HTMLMemes}</ul>;
 }
