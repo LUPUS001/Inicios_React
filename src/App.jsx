392aefed95c87a65be0bc78ec7f7f307ba4825fa
LUPUS001 <antoniodaw23@gmail.com>
2026-03-06 10:51:53 +0100

    Proyecto práctico: Introducción

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,12 @@
-import { useEffect, useState } from "react";
 import "./App.css";
-import HeaderComponent from "./components/HeaderComponent";
-import Login from "./components/Login";
-import MemeList from "./components/MemeList";
+import PokemonList from "./components/PokemonList";
 
 function App() {
-  const [greetings, setGreetings] = useState("Bienvenidos a mi web!");
-  const links = {
-    home: "Home",
-    blog: "Blog",
-    news: "News",
-    contact: "Contact us",
-  };
-
-  const [user, setUser] = useState({});
-
-  const login = (userInfo) => {
-    setUser(userInfo);
-  };
-
-  const [showMovies, setShowMovies] = useState(true);
-
   return (
     <>
-      <HeaderComponent greetings={greetings} links={links}></HeaderComponent>
+      <h2>Pokemon Project</h2>
 
-      <main className="main-content">
-        <MemeList></MemeList>
-      </main>
+      <PokemonList></PokemonList>
     </>
   );
 }
--- src/components/ButtonComponent.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function ButtonComponent() {
-  return (
-    <button>Soy un botón</button>
-  )
-}
-
-export default ButtonComponent
