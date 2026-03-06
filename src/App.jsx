bf83c658d8807f243473baaef138aa719f9dc493
LUPUS001 <antoniodaw23@gmail.com>
2026-03-06 08:35:09 +0100

    Trabajando con Formularios y API: Llamadas a API

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import "./App.css";
 import HeaderComponent from "./components/HeaderComponent";
 import Login from "./components/Login";
-import MovieList from "./components/MovieList";
+import MemeList from "./components/MemeList";
 
 function App() {
   const [greetings, setGreetings] = useState("Bienvenidos a mi web!");
@@ -21,25 +21,12 @@ function App() {
 
   const [showMovies, setShowMovies] = useState(true);
 
-  // Este hook se ejecutará con cada renderizado (Updating sin array)
-  /* useEffect(() => {
-    console.log("Ejecución cada vez que se renderiza el componente raíz");
-  }); */
-
-  // Este hook solamente se ejecutará si la variable user cambia (Updating con array)
-  /* useEffect(() => {
-    console.log("Ejecución con cada cambio de la variable reactiva user");
-  }, [user]); */
-  /* Al poner un array específicamos con que se actualizará y no se actualizará con todos los renderizados */
-
   return (
     <>
       <HeaderComponent greetings={greetings} links={links}></HeaderComponent>
 
       <main className="main-content">
-        <h2>Hola {user.username}</h2>
-
-        <Login handleLogin={login}></Login>
+        <MemeList></MemeList>
       </main>
     </>
   );
